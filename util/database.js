93b156ca3e9a420c49f083770213246dc648300d
rishabsingla5@gmail.com <rishabsingla5@gmail.com>
2023-12-01 20:06:24 +0530

    usingSequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'product',
-    password: 'localHost@123'
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-
-module.exports = pool.promise();
+module.exports = sequelize;
